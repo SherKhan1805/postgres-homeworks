335cb9f7c65227a42cf8da24ecdca6e427e8acff
Oleg Usachev <usaechv97@mail.ru>
2023-10-08 19:09:51 +0500

    В файлах alter_northwind.sql и alter_student.sql мы прописали запросы для редактирования таблиц

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,33 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+CREATE TABLE student
+(
+	student_id serial,
+	first_name varchar,
+	last_name varchar,
+	birthday date,
+	phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student RENAME birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student (first_name, last_name, birthday, phone)
+VALUES
+    ('Иван', 'Иванов', '2000-01-01', '8-800-555-35-35'),
+    ('Петя', 'Петров', '2001-02-02', '8-800-666-36-36'),
+    ('Александр', 'Александров', '2002-03-03', '8-800-777-37-37');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+DELETE FROM student;
+
+ALTER SEQUENCE student_student_id_seq RESTART WITH 1;
